b0c83fc52e9dc7494e9412126f4505918ba06261
Maryia_Nabzdorava <nabzdorova@gmail.com>
2020-11-09 03:14:25 +0300

    [Task 4]: "Cloud Databases Overview Overview of DB solutions. Infrastructure As Code (IaC), deployment sample to deploy RDS": fix column name

--- product-service/constants/sql.ts
+++ product-service/constants/sql.ts
@@ -1,4 +1,4 @@
 export const PRODUCTS = 'SELECT products.id, products.title, products.description, products.price, stocks.count from products INNER JOIN stocks ON products.id=stocks.product_id';
 export const PRODUCT_BY_ID = 'SELECT products.id, products.title, products.description, products.price, stocks.count from products INNER JOIN stocks ON products.id=stocks.product_id WHERE products.id = $1::uuid';
 export const INSERT_PRODUCT = 'INSERT INTO products(title, description, price) VALUES($1::text, $2::text, $3::integer) RETURNING id';
-export const INSERT_STOCK = 'INSERT INTO stocks(product_id, countsr) VALUES($1::uuid, $2::integer) RETURNING id';
+export const INSERT_STOCK = 'INSERT INTO stocks(product_id, count) VALUES($1::uuid, $2::integer) RETURNING id';
